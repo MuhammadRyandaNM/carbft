77d03c4d6dd109ee6bc5fc55b87ecf6648d1382e
MuhammadRyandaNM <muhammadryandanm@gmail.com>
2022-11-10 16:48:07 +0800

    royalties

---
+++ royalties/nstoken.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.4;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract NSToken is ERC20 {
+
+    constructor() ERC20("NSToken", "NST") {
+        _mint(msg.sender, 1000000);
+    }
+
+}
